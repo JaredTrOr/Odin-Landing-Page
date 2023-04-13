165b9e260a9ae98492ca3615f692e527d4fa0734
JaredTrOr <trujillojaredalexander@gmail.com>
2023-04-12 22:27:31 -0600

    Add space to the top container, reduce the padding top, add the js code to open an url using buttons, there was changed the text of the quote

---
+++ script.js
@@ -0,0 +1,9 @@
+
+/*
+I could've added an anchor tag instead of adding this Javascript code but
+I don't know, I'm experimenting here
+*/
+document.querySelectorAll('.btn').forEach(btn => btn.addEventListener('click', () => {
+    window.open('https://github.com/JaredTrOr', '_blank')
+}));
+
